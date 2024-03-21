5063d655e0afe6fb516d9c70011a02c57872775c
Vitor Mateus <vitor7jan@gmail.com>
2024-03-21 20:58:23 -0300

    criado contador de acessos

--- service-worker.js
+++ service-worker.js
@@ -6,13 +6,25 @@ const updateBlacklist = async () => {
   blacklist = json.results.map((item) => new URL(item.url).host);
 };
 
+const createCounter = async () => {
+  const { counter } = await chrome.storage.local.get("counter");
+  if (counter) return;
+  await chrome.storage.local.set({ counter: 1 });
+};
+
 updateBlacklist();
+createCounter();
 
 chrome.runtime.onStartup.addListener(async () => updateBlacklist());
 
-chrome.tabs.onUpdated.addListener((_, _, tab) => {
+chrome.tabs.onUpdated.addListener(async (_tabId, _changeInfo, tab) => {
   if (tab.status != "complete") return;
   const domain = new URL(tab.url).host;
   if (!blacklist.includes(domain)) return;
-  chrome.tabs.create({ url: "https://www.youtube.com/@primorico" });
+
+  const { counter } = await chrome.storage.local.get("counter");
+  chrome.tabs.create({
+    url: `https://www.youtube.com/@primorico?counter=${counter}`,
+  });
+  await chrome.storage.local.set({ counter: counter + 1 });
 });
