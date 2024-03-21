55309378ef55778ade86d94c0c702d90c5e521cf
Vitor Mateus <vitor7jan@gmail.com>
2024-03-20 22:01:44 -0300

    adicionada callback que verifica url acessada

--- service-worker.js
+++ service-worker.js
@@ -5,3 +5,10 @@ chrome.runtime.onStartup.addListener(async () => {
   const json = await response.json();
   blacklist = json.results.map((item) => new URL(item.url).host);
 });
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (tab.status != "complete") return;
+  const domain = new URL(tab.url).host;
+  if (!blacklist.includes(domain)) return;
+  chrome.tabs.create({ url: "https://www.youtube.com/@primorico" });
+});
