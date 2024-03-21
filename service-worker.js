06585cba6be3664bf721e9008d36b0b3abfb9da7
Vitor Mateus <vitor7jan@gmail.com>
2024-03-21 20:46:17 -0300

    criada funcao update blacklist

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,16 @@
 let blacklist = [];
 
-chrome.runtime.onStartup.addListener(async () => {
+const updateBlacklist = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/item/");
   const json = await response.json();
   blacklist = json.results.map((item) => new URL(item.url).host);
-});
+};
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+updateBlacklist();
+
+chrome.runtime.onStartup.addListener(async () => updateBlacklist());
+
+chrome.tabs.onUpdated.addListener((_, _, tab) => {
   if (tab.status != "complete") return;
   const domain = new URL(tab.url).host;
   if (!blacklist.includes(domain)) return;
