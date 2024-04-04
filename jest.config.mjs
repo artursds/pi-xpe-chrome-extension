4b3a815f4e6c63a2494a87defd896217788dbe02
Vitor Mateus <vitor7jan@gmail.com>
2024-04-03 21:48:22 -0300

    configurado jest

---
+++ jest.config.mjs
@@ -0,0 +1,10 @@
+const config = {
+  bail: 1,
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  rootDir: "test",
+  coverageProvider: "v8",
+};
+
+export default config;
