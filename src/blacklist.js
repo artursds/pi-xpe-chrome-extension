689e14ea3f9e889c6a0e412967b8695920c80794
Vitor Mateus <vitor7jan@gmail.com>
2024-04-09 18:59:05 -0300

    url da api sendo buscada do repositorio

--- src/blacklist.js
+++ src/blacklist.js
@@ -1,10 +1,15 @@
-const API_URL = "http://localhost:8000/api";
+const REPOSITORY_URL =
+  "https://vitor-msp.github.io/pa-xpe-repository/api-url.json";
+let API_URL;
 let blacklist = [];
 
 const updateBlacklist = async () => {
   try {
-    const response = await fetch(`${API_URL}/blacklist`);
-    const content = await response.json();
+    let response = await fetch(REPOSITORY_URL);
+    let content = await response.json();
+    API_URL = content.apiUrl;
+    response = await fetch(`${API_URL}/blacklist`);
+    content = await response.json();
     blacklist = content.domains;
   } catch (error) {
     blacklist = [];
