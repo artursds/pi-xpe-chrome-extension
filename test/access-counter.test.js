179b5e542ea1a5cdc0c0428b67a720d5a57831cb
Vitor Mateus <vitor7jan@gmail.com>
2024-04-12 23:31:34 -0300

    refatorados testes no modulo access counter

--- test/access-counter.test.js
+++ test/access-counter.test.js
@@ -20,45 +20,32 @@ const mockLocalStorage = (getReturnValue) => {
 describe("create access counter tests", () => {
   test("should create access counter setted with 1", async () => {
     mockLocalStorage(undefined);
-    jest.spyOn(chrome.storage.local, "set");
-
     await createAccessCounter();
-
-    const argument = chrome.storage.local.set.mock.calls[0][0];
-    expect(argument).toEqual({ accessCounter: 1 });
+    expect(chrome.storage.local.set).toHaveBeenCalledTimes(1);
+    expect(chrome.storage.local.set).toHaveBeenCalledWith({ accessCounter: 1 });
   });
 
   test("should not create access counter when it already exists", async () => {
     mockLocalStorage({ accessCounter: 1 });
-    jest.spyOn(chrome.storage.local, "set");
-
     await createAccessCounter();
-
-    const calls = chrome.storage.local.set.mock.calls;
-    expect(calls).toEqual([]);
+    expect(chrome.storage.local.set).toHaveBeenCalledTimes(0);
   });
 });
 
 describe("next access counter tests", () => {
   test("should return and save 1 when access counter is undefinded", async () => {
     mockLocalStorage(undefined);
-    jest.spyOn(chrome.storage.local, "set");
-
     const accessCounter = await nextAccessCounter();
-
     expect(accessCounter).toBe(1);
-    const argument = chrome.storage.local.set.mock.calls[0][0];
-    expect(argument).toEqual({ accessCounter: 2 });
+    expect(chrome.storage.local.set).toHaveBeenCalledTimes(1);
+    expect(chrome.storage.local.set).toHaveBeenCalledWith({ accessCounter: 2 });
   });
 
   test("should return access counter and save it incremented by 1", async () => {
     mockLocalStorage({ accessCounter: 5 });
-    jest.spyOn(chrome.storage.local, "set");
-
     const accessCounter = await nextAccessCounter();
-
     expect(accessCounter).toBe(5);
-    const argument = chrome.storage.local.set.mock.calls[0][0];
-    expect(argument).toEqual({ accessCounter: 6 });
+    expect(chrome.storage.local.set).toHaveBeenCalledTimes(1);
+    expect(chrome.storage.local.set).toHaveBeenCalledWith({ accessCounter: 6 });
   });
 });
